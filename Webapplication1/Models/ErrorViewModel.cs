c5ef32f41a4f99a76e8d94f0b5687f733b9a99a1
KHACMANH-PC\admin <manhkhacnguyen1997@gmail.com>
2018-08-27 20:21:25 +0700

    Add project files.

---
+++ Webapplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webapplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
